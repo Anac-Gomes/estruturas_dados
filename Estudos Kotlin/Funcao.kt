a8e413d2235cef29caf337f7f07c81e4d1516eb4
Ana Carolina Gomes <162213007+Anac-Gomes@users.noreply.github.com>
2024-03-13 23:33:07 -0300

    Add files via upload

---
+++ Estudos Kotlin/Funcao.kt
@@ -0,0 +1,12 @@
+fun main() {
+   var number1 = 5000
+   var number2 = 120
+
+   var resultado = somar(number1,number2)
+   println( resultado )
+
+   }
+   fun somar(number1: Int, number2: Int): Int {
+    var total = number1 + number2
+    return total
+}
